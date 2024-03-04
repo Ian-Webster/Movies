a622ec48b9ecc26c687d63ea05510b9f2656237f
Webster <i_web_99@yahoo.com>
2024-03-04 14:07:48 +0000

    #2 rework non admin views

---
+++ ux/Movies.Ux.Mvc/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Movies.Business.Interfaces;
+using Movies.Domain.Enums;
+
+namespace Movies.Ux.Mvc.Controllers
+{
+
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        // GET: MovieController
+        public async Task<ActionResult> Index()
+        {
+            var movies = await _movieService.GetMoviesAsync();
+            return View(movies);
+        }
+
+        // GET: MovieController/Details/5
+        public async Task<ActionResult<Domain.DTO.Movie>> Details(Guid id)
+        {
+            try
+            {
+                var movie = await _movieService.GetMovieAsync(id);
+                return View(movie);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception here
+                Console.WriteLine(ex);
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+    }
+}
